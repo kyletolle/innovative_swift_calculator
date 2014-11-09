58d9237bf5a9eb019c99d8165b1a1a7c3c402d45
Kyle Tolle <kyle@nullsix.com>
2014-11-08 20:26:33 -0700

    Initial Commit

---
+++ innovative_swift_calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  innovative_swift_calculator
+//
+//  Created by Kyle Tolle on 11/8/14.
+//  Copyright (c) 2014 Kyle Tolle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ innovative_swift_calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  innovative_swift_calculator
+//
+//  Created by Kyle Tolle on 11/8/14.
+//  Copyright (c) 2014 Kyle Tolle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
